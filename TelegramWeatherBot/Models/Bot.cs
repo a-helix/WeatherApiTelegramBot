573202cb06c44ba897ee4cb9c31f85e68aa87b7e
Oleksandr <sorokin.biochemistry@gmail.com>
2020-11-06 21:10:45 +0100

    first commit

---
+++ TelegramWeatherBot/Models/Bot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramWeatherBot.Models
+{
+    public class Bot
+    {
+        private TelegramBotClient client = new TelegramBotClient();
+
+        public async Task<TelegramBotClient> Get()
+        {
+            if(client != null)
+            {
+                return client;
+            }
+        }
+    }
+}
