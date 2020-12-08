d80368ca0f6692b0b42afc6a6d7141a6ce45bb29
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-08 22:42:49 +0100

    Added logger to build.sh

--- TelegramWeatherBot/build.sh
+++ TelegramWeatherBot/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo rm -r src
+echo "Building TelegramWeatherBot..."
 dotnet build TelegramWeatherBot.sln -c Release -o "src/"
+echo "TelegramWeatherBot has been built."
+echo "Building container...."
 docker build Dockerfile -t bot
+echo "Container has been built."
