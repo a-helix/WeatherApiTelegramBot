df9013752de35431b833b2415baa31c920a7d317
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-09 17:52:11 +0100

    minor fix in build.sh

--- Deploy/build.sh
+++ Deploy/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo rm -r src
 echo "Building TelegramWeatherBot..."
+cd ..
 dotnet build TelegramWeatherBot.sln -c Release -o "src/"
 echo "TelegramWeatherBot has been built."
 echo "Building container...."
