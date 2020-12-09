371f4255e64bac54cbc4d7cddb171068c3ac3e94
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-09 18:25:35 +0100

    deploy update

--- Deploy/build.sh
+++ Deploy/build.sh
@@ -5,5 +5,6 @@ cd ..
 dotnet build TelegramWeatherBot.sln -c Release -o "src/"
 echo "TelegramWeatherBot has been built."
 echo "Building container...."
+cd Deploy
 docker build Dockerfile -t bot
 echo "Container has been built."
